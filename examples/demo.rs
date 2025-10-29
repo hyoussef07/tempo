c1753560b4492f69e9f421310bb9f925f4ac5fb9
hyoussef07 <hiyoussef077@gmail.com>
2025-10-29 15:40:00 +0100

    Add usage examples

---
+++ examples/demo.rs
@@ -0,0 +1,51 @@
+use tempo::{dt, Duration, DateTime};
+
+fn main() {
+    #[cfg(feature = "tz")]
+    {
+        let ny = dt()
+            .set_zone("America/New_York")
+            .minus(&Duration::from_object(&[("weeks", 1)]))
+            .end_of("day");
+
+        println!("With timezone:");
+        println!("  ISO: {}", ny.to_iso());
+        println!("  Format: {}", ny.to_format("MMMM do, yyyy"));
+        println!("  Locale: {}", ny.to_locale_string(DateTime::DATE_FULL));
+        println!();
+    }
+
+    let result = dt()
+        .plus(&Duration::from_object(&[("days", 3), ("hours", 2)]))
+        .start_of("day")
+        .to_format("yyyy-MM-dd HH:mm:ss");
+
+    println!("Chainable demo:");
+    println!("  3 days + 2 hours from now, start of day: {}", result);
+    println!();
+
+    let dur = Duration::from_object(&[("weeks", 2), ("days", 3)]);
+    println!("Duration demo:");
+    println!("  2 weeks + 3 days = {} days", dur.as_unit("days"));
+    println!("  Object: {:?}", dur.to_object());
+    println!();
+
+    let now = dt();
+    println!("Format tokens:");
+    println!("  yyyy-MM-dd: {}", now.to_format("yyyy-MM-dd"));
+    println!("  MMMM do, yyyy: {}", now.to_format("MMMM do, yyyy"));
+    println!("  EEEE at h:mm a: {}", now.to_format("EEEE 'at' h:mm a"));
+    println!();
+
+    println!("Locale presets:");
+    println!("  DATE_SHORT: {}", now.to_locale_string(DateTime::DATE_SHORT));
+    println!("  DATE_MED: {}", now.to_locale_string(DateTime::DATE_MED));
+    println!("  DATE_FULL: {}", now.to_locale_string(DateTime::DATE_FULL));
+    println!("  TIME_SIMPLE: {}", now.to_locale_string(DateTime::TIME_SIMPLE));
+    println!("  DATETIME_SHORT: {}", now.to_locale_string(DateTime::DATETIME_SHORT));
+    println!();
+
+    println!("Start/end of units:");
+    println!("  Start of month: {}", now.clone().start_of("month").to_format("yyyy-MM-dd HH:mm:ss"));
+    println!("  End of day: {}", now.clone().end_of("day").to_format("yyyy-MM-dd HH:mm:ss.SSS"));
+}
---
+++ examples/timezone.rs
@@ -0,0 +1,66 @@
+use tempo::{dt, DateTime, Duration, Interval};
+
+fn main() {
+    println!("=== Tempo Timezone Example ===\n");
+
+    let utc_now = dt();
+    println!("Current UTC time:");
+    println!("  {}", utc_now.to_iso());
+    println!("  {}", utc_now.to_format("EEEE, MMMM do yyyy, h:mm:ss a"));
+    println!();
+
+    #[cfg(feature = "tz")]
+    {
+        let zones = vec![
+            "America/New_York",
+            "Europe/London",
+            "Asia/Tokyo",
+            "Australia/Sydney",
+        ];
+
+        println!("Same moment in different timezones:");
+        for zone in zones {
+            let tz_time = dt().set_zone(zone);
+            println!("  {}: {}", zone, tz_time.to_iso());
+        }
+        println!();
+
+        let meeting = dt()
+            .set_zone("Europe/Paris")
+            .plus(&Duration::from_object(&[("weeks", 2)]))
+            .start_of("day")
+            .plus(&Duration::from_object(&[("hours", 15)]));
+
+        println!("Meeting in 2 weeks at 3 PM Paris time:");
+        println!("  Paris: {}", meeting.to_format("EEEE, MMMM do 'at' h:mm a"));
+        println!();
+    }
+
+    #[cfg(not(feature = "tz"))]
+    {
+        println!("⚠️  Timezone features disabled. Enable with --features tz");
+        println!();
+    }
+
+    let start = dt();
+    let end = start.clone().plus(&Duration::from_object(&[("days", 30)]));
+    let interval = Interval::from_date_times(start.clone(), end);
+
+    println!("Interval example (next 30 days):");
+    println!("  Start: {}", start.to_format("MMM d, yyyy"));
+    println!("  End: {}", interval.end().to_format("MMM d, yyyy"));
+    println!("  Length: {} days", interval.length("days").as_unit("days"));
+
+    let check_date = dt().plus(&Duration::from_object(&[("days", 15)]));
+    println!("  Contains +15 days? {}", interval.contains(&check_date));
+    let future = dt().plus(&Duration::from_object(&[("days", 100)]));
+    println!("  Contains +100 days? {}", interval.contains(&future));
+    println!();
+
+    let past = DateTime::from_iso("2020-01-01T00:00:00Z").unwrap();
+    let diff_days = utc_now.diff(&past, "days");
+    let diff_years = utc_now.diff(&past, "years");
+    println!("Time since 2020-01-01:");
+    println!("  {:.0} days", diff_days);
+    println!("  {:.2} years", diff_years);
+}
